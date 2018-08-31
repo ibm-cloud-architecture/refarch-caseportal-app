052150a46fadec692516a7ba8db51228db4c8c5a
Jerome Boyer <boyerje@us.ibm.com>
2018-08-31 11:35:49 -0700

    Change chart

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,13 +11,22 @@ prev=$(grep -o 'v\([0-9]\+.\)\{2\}\([0-9]\+\)' server/config/config.json | head 
 
 if [[ $# -gt 0 ]]; then
 	v=v$1
+  sed -i -e "s/$prev/$v/g" server/config/config.json
+  rm server/config/config.json-e
 else
   v=$prev
 fi
 echo 'old version: ' $prev ' new version to use:' $v
-echo 'Update version in config file'
-sed -i -e "s/$prev/$v/g" server/config/config.json
-rm server/config/config.json-e
 
 echo 'compile angular app'
-ng build
+ng build --prod
+echo 'Build Docker image'
+# Build docker
+docker build -t ibmcase/casewebportal .
+read -p 'Push to dockerhub?: (yes)/no' rep
+if [ -z "$rep" ]
+then
+      echo "Pushing..."
+      docker login
+      docker push ibmcase/casewebportal:latest
+fi
--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Set shared variables
 export progname=portal-app
-export k8cluster=green2-cluster.icp
-export namespace=browncompute
-export helmchart=browncompute-portal-app
+export k8cluster=green-cluster
+export namespace=greencompute
+export helmchart=bc-caseportal-app
